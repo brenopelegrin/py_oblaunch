7c2f34ddd8628822c8fd15a89a8d2bbfab32d825
Breno Henrique <breno.henrique@aluno.ifsp.edu.br>
2020-04-19 16:10:01 -0300

    v3
    Adicionada a resistência do ar (possui bugs)

--- dev/muv.py
+++ dev/muv.py
@@ -4,26 +4,37 @@ import matplotlib.pyplot as plt
 import math
 
 g = 9.81      # m/s²
-N = 20000     # número de passos no loop
-height = 50    # Initial height (0 = ground)
-iv = 0      # Initial velocity
+N = 20000     # Number of steps in the loop
+height = 500  # Initial height in meters (0 = ground)
+iv = 0        # Initial velocity in m/s
+m = 10        # Mass in kg
+b = 0.5       # Drag constant
+# ---- para casos futuros ----#
+ap = 1.275    # Air pressure in kg/m³
+d = 0.5       # Drag coefficient
+area = 0.0318 # Area in meters
+fr = -0.5*ap*d*area # Must be multiplied by v^2 in the step
+#-----------------------------#
+graph = 2     #(Debug) Define quais gráficos serão plotados. 0 para apenas teórico, 1 para apenas Euler e 2 para ambos.
+error = 0     #(Debug) Define se o erro será plotado. 0 para não, 1 para sim.
 
 #Nota: neste algoritmo, adotamos a trajetória para cima como positiva. 
     
 def compare(a, b, dt, time):
-    error=subtract(a, b)
-    print("Ultimo erro para dt={} : {}".format(dt, error[len(error)-1]))
-    plt.figure(2) #Define a figura 2
-    plt.plot(time, error[:,0], label='Δt = '+str(dt)+' s') #Plota o gráfico de erro x tempo na figura 2
+    global error
+    if error == 1:
+        error=subtract(a, b)
+        plt.figure(2) #Define a figura 2
+        plt.plot(time, error[:,0], label='Δt = '+str(dt)+' s') #Plota o gráfico de erro x tempo na figura 2
 
-def theoricf(t):
-    y = (height+(iv*t))-(g*t*t/2)
-    v = iv-(g*t)
+def theoricf(t, a):
+    v = iv+(a*t)
+    y = (height+(iv*t))+(a*t*t/2)
     return array([y, v])
 
-def step(s, dt):
+def step(s, a, dt):
     y = s[0] +s[1]*dt
-    v = s[1] -g*dt
+    v = s[1] +a*dt
     return array([y, v])
 
 plt.figure(1)
@@ -32,7 +43,10 @@ def main():
     arraydt=[0.2, 0.1, 0.01, 0.001] #Define a matriz de dt
     
     for dt in arraydt:
+        global g, graph
         time=zeros([N])
+        ae=zeros([N], float)
+        ae[0]=-g
         theoric=zeros([N, 2], float)
         mat=zeros([N, 2], float)
         mat[0] = height,iv
@@ -44,16 +58,28 @@ def main():
         #Sintaxe de chamada: matriz[linha, coluna] = y
         j=0
         while mat[j,0]>=0 and j<=(N-2):
-            mat[j+1] = step(mat[j], dt)       
             time[j+1] = time[j]+dt
-            theoric[j+1] = theoricf(time[j+1]) #chama a função teorico e passa o tempo
+            #------ metodos de aceleração (teste) -----#
+            ae = (-b*mat[j,1]/m)-g #aceleração (simulado) 
+            at = (-b*theoric[j,1]/m)-g #aceleração (teórico)
+            #at = -g*(exp(-b*time[j]/m)) #metodo teorico da função a(t)
+            #ae[j+1] = ae[j] - (b*g/(exp(b*dt/m))*m)*dt #metodo de euler da função a(t)
+            #------------------------------------------#
+            mat[j+1] = step(mat[j], ae, dt)
+            theoric[j+1] = theoricf(time[j+1], at) #chama a função teorico e passa o tempo
             j+=1
         time=time[:j]
         mat=mat[:j]
+        #if dt == 0.001 or dt == 0.01:
+            #for x in range(len(a)-1):
+                #if a[x] != 0:
+                    #print(a[x])
         theoric=theoric[:j]
         plt.figure(1)
-        plt.plot(time, mat[:,0], label='Integração (dt = '+str(dt)+" s)") #Plota a matriz simulada na figura 1
-        plt.plot(time, theoric[:,0], label='Teórico (dt = '+str(dt)+" s)") #Plota a matriz simulada na figura 1
+        if graph == 1 or graph == 2:
+            plt.plot(time, mat[:,0], label='Integração (dt = '+str(dt)+" s)") #Plota a matriz simulada na figura 1
+        if graph == 0 or graph == 2:
+            plt.plot(time, theoric[:,0], label='Teórico (dt = '+str(dt)+" s)") #Plota a matriz simulada na figura 1
         compare(mat, theoric, dt, time) #Chama a funcao de comparacao
         
     ############## CONFIGURAÇÃO DOS GRÁFICOS ##########
@@ -68,10 +94,11 @@ def main():
         plt.title("Queda livre (MUV)")
     plt.legend()
     
-    plt.figure(2)
-    plt.xlabel("Tempo (s)")
-    plt.ylabel("Erro (m)")
-    plt.title("Erro (Diferença Integração x Teórico)")
+    if error == 1:
+        plt.figure(2)
+        plt.xlabel("Tempo (s)")
+        plt.ylabel("Erro (m)")
+        plt.title("Erro (Diferença Integração x Teórico)")
     plt.legend()
     
     plt.show()
